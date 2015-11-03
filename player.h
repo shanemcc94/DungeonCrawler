b35acc94e1ac92ad6d6d629b8d7062b1d960e5db
Shane McCausland <shanemcc94@gmail.com>
2015-11-03 12:57:57 +0000

    Create player.h

---
+++ player.h
@@ -0,0 +1,36 @@
+#ifndef PLAYER
+#define PLAYER
+
+#include <QGraphicsPixmapItem>
+#include <QKeyEvent>
+
+#include "arrow.h"
+
+class Player: public QGraphicsPixmapItem, QObject
+{
+
+private:
+    int playerType;
+
+public:
+
+    Player();
+    Arrow* arrow;
+    void keyPressEvent(QKeyEvent * event);
+    void move(int newX, int newY);
+    void setPlayerType(int NewPlayerType);
+    int getPlayerType();   
+
+/*private:
+    QPixmap playerUp;
+    QPixmap playerDown;
+    QPixmap playerLeft;
+    QPixmap playerRight;*/
+
+};
+
+
+
+
+#endif // PLAYER
+
